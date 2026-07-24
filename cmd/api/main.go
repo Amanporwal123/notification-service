eded2b3f7343a036ea616b57db77270ca16166c3
Aman Kala <aman.kala@tudip.com>
2026-07-25 01:27:10 +0530

    Implement graceful shutdown for the HTTP server using context and signal handling

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Amanporwal123/notification-service/internal/config"
 	"github.com/Amanporwal123/notification-service/pkg/logger"
@@ -17,31 +22,48 @@ func main() {
 	if err := logger.InitLogger(); err != nil {
 		log.Fatalf("failed to initialize logger: %v", err)
 	}
-	defer logger.Sync() // Ensure logs are flushed before the program exits
+	defer logger.Sync()
 	logger.Log.Info("Logger initialized successfully")
 
-	// Load Configuration
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		logger.Log.Fatal("failed to load config", zap.Error(err))
 	}
 	logger.Log.Info("Configuration loaded", zap.String("port", cfg.Server.Port))
 
-	//Initialize Gin Router
 	r := gin.Default()
 
-	//Setup Basic Route (Healthcheck)
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	//Start HTTP Server
-	addr := ":" + cfg.Server.Port
-	logger.Log.Info("Starting HTTP server", zap.String("address", addr))
 
-	if err := r.Run(addr); err != nil {
-		logger.Log.Fatal("failed to start server", zap.Error(err))
+	addr := ":" + cfg.Server.Port
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: r,
 	}
 
+	go func() {
+		logger.Log.Info("Starting HTTP server", zap.String("address", addr))
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Log.Fatal("Listen failed", zap.Error(err))
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+	logger.Log.Info("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Log.Fatal("Server forced to shutdown:", zap.Error(err))
+	}
+
+	logger.Log.Info("Server exiting")
 }
